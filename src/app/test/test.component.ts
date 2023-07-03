6fdb91c8b5cbe7b2ee01d640277e61df05345fed
kerfiguetteb <kerfiguetteboutman@gmail.com>
2023-07-03 16:13:44 +0200

    feat: ajout composent

---
+++ src/app/test/test.component.ts
@@ -0,0 +1,16 @@
+import {Component } from '@angular/core';
+import {MatIconModule} from '@angular/material/icon';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatButtonModule} from '@angular/material/button';
+
+@Component({
+  selector: 'app-test',
+  templateUrl: './test.component.html',
+  styleUrls: ['./test.component.scss'],
+  standalone: true,
+  imports: [MatButtonModule, MatDividerModule, MatIconModule],
+})
+export class TestComponent {
+  title = 'test';
+
+}
